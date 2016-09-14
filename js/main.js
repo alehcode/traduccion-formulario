3cd865e83cf0f17b8ca149748e6b246cf0e044e7
Alexandra Lopez <sccp.alopez@gmail.com>
2016-09-14 13:54:10 -0500

    subiendo en español

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesión";
+document.getElementById("inputEmail").placeholder="Correo Electronico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML="Recordar Datos";
+document.querySelector("button").innerHTML="Iniciar Sesión";
